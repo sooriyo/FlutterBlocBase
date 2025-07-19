738c07802ad9e4d24800f4d09ca087a91c766fa5
Soori <tharuka@maximimpressions.com>
2025-07-19 10:34:59 +0530

    Moved assets: - .env under flutter: to ensure the .env file is recognized as an asset by Flutter’s build system.

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,4 +1,7 @@
+import '../../config/environment/env_config.dart';
+
 class ApiConstants {
+  static String get baseUrl => EnvConfig.apiBaseUrl;
   static const int connectTimeout = 30000; // milliseconds
   static const int receiveTimeout = 30000; // milliseconds
 }
